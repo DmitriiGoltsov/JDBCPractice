77a9d1009f3902a7060fb32f60dfc64ccac3036f
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-07-20 18:19:11 +0200

    Trained usage of executeUpdate() method.

--- jdbc-starter/src/com/dvg/jdbc/starter/JdbcRunner.java
+++ jdbc-starter/src/com/dvg/jdbc/starter/JdbcRunner.java
@@ -15,18 +15,19 @@ public class JdbcRunner {
 
         Class<Driver> driverClass = Driver.class;
         String sqlInquiry = """
-                CREATE TABLE IF NOT EXISTS info (
-                    id SERIAL PRIMARY KEY ,
-                    data TEXT NOT NULL 
-                );
+                UPDATE info
+                SET data = 'TestTest'
+                WHERE id = 7
+                RETURNING *;
                 """;
 
         try (Connection psqlConnection = openConnection();
              Statement statement = psqlConnection.createStatement()) {
             System.out.println(psqlConnection.getSchema());
             System.out.println(psqlConnection.getTransactionIsolation());
-            boolean result = statement.execute(sqlInquiry);
+            int result = statement.executeUpdate(sqlInquiry);
             System.out.println(result);
+            System.out.println(statement.getUpdateCount());
         }
 
     }
